9da829aac21f6a738936d875a693fa922f73d6cc
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-17 18:22:10 +0530

    add logout route

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("members/add_new_member", views.add_new_member, name="add_new_member"),
     path("members/edit/<int:id>", views.update_member_details, name="edit"),
     path("members/delete/<int:id>", views.delete_member, name="delete"),
+    path('logout/', views.logout_user, name='logout_user')
+
 ]
