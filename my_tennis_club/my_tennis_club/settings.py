0cba63db30a6f8a50fc7fbc36bfc6c340365d25d
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-04 23:04:33 +0530

    add STATICFILES_DIRS

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,8 +149,9 @@ STATIC_ROOT = BASE_DIR / "productionfiles"
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / "mystaticfiles"]
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
