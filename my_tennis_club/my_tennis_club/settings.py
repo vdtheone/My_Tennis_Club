69a999dbbacf22e755e44f9d10bbb1bd0b6122a2
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-05 15:47:48 +0530

    change logout url to main

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,4 +163,4 @@ AUTHENTICATION_BACKENDS = (
 SOCIALACCOUNT_LOGIN_ON_GET = True  # stop to open an intermediate Allaut interface
 
 LOGIN_REDIRECT_URL = "/members"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "main"
